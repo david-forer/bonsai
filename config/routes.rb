574b1a799ea4cf16daf5f65db1a91c7b5c8f33f5
david-forer <david.j.forer@gmail.com>
2020-02-13 12:02:07 +0900

    add static pages about, contact, help & controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  resources :bonsai_items
-  resources :carts
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/help'
+  # resources :bonsai_items
+  # resources :carts
   resources :bonsais
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
+  get 'bonsais/add_to_cart/:id', to:'bonsais#add_to_cart', as: 'add_to_cart'
+  get 'bonsais/remove_from_cart/:id', to: 'bonsais#remove_from_cart', as: 'remove_from_cart'
+  # delete 'bonsais/remove_from_cart/:id', to: 'bonsais#remove_from_cart', as: 'remove_from_cart'
 
-
-  root 'bonsais#index'
+  root :to => 'bonsais#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
